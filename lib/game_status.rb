90fa02e744309e46e1fb5c18b520fee9fa3e5a31
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 20:33:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,12 +6,12 @@ end
 # Define your WIN_COMBINATIONS constant
 
 def win_combinations [
-  [0,1,2], #top row
-  [3,4,5], #middle row
-  [6,7,8], #bottom row
-  [0,3,6], #left column
-  [1,4,7], #center column
-  [2,5,8], #right column
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
   [0,4,8],
   [2,4,6]
 ]
