adbbc88b1ff3d3dc01e2917307c116a12676e076
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 21:27:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,7 +23,6 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
   binding.pry
-    return
     win_combination[0] = win_index_1
     win_combination[1] = win_index_2
     win_combination[2] = win_index_3
