4c229c6320aaf24b3a9b5c776324916559923bcf
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 20:57:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,9 +29,9 @@ def won(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  else
-    false
-  end
+      return win_combination
+    else
+      false
+    end
   end
 end
