bc9c585aafad5b54ca78418459ac972a9f396d57
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 20:56:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,5 +23,14 @@ def won(board)
     win_index_1 = win_combination[0]
     win_index_1 = win_combination[1]
     win_index_1 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
   end
 end
