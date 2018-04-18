2ab917cf571aec78cc62246d5e477332bf44e8fa
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 21:42:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,20 +21,16 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.find do |win_combination|
   binding.pry
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-    if  position_1 == position_2 && position_2 == position_3 && position_taken?(board, position_1)
-      win_combination
-    else
-      false
-    end
+    position_1 == position_2 && position_2 == position_3 && position_taken?(board, position_1)
   end
 end
