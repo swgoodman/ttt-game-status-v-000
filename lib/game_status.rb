a984d6be59b62e1b92a1ad12cbbb1921ec8cad3a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 20:32:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,14 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+def win_combinations [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #left column
+  [1,4,7], #center column
+  [2,5,8], #right column
+  [0,4,8],
+  [2,4,6]
+]
