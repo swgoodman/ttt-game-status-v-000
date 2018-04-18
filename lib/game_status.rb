ddffdd6e19de0cdf59b9e8496b1d013fecd4ffd6
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-18 22:44:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ end
 def winner(board)
   over?(board)
   won?(board)
-    WIN_COMBINATIONS.find? do |win_combination|
+    WIN_COMBINATIONS.find do |win_combination|
       win_index_1 = win_combination[0]
       if win_index_1 == "X"
         "X"
